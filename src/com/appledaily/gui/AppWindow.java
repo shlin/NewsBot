069c099bc13fb06fb38b5051fd39612454a72b6c
PStar <shlin65@me.com>
2017-06-17 01:06:33 +0800

    新增下載間隔參數、顯示搜尋結果總數

--- src/com/appledaily/gui/AppWindow.java
+++ src/com/appledaily/gui/AppWindow.java
@@ -8,7 +8,6 @@ package com.appledaily.gui;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -25,8 +24,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import jdbc.mysql.MySQLConnector;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
 
 /**
  *
@@ -55,6 +52,7 @@ public class AppWindow extends javax.swing.JFrame {
 	 */
 	// @SuppressWarnings("unchecked")
 	// <editor-fold defaultstate="collapsed" desc="Generated
+	// <editor-fold defaultstate="collapsed" desc="Generated
 	// Code">//GEN-BEGIN:initComponents
 	private void initComponents() {
 
@@ -66,6 +64,146 @@ public class AppWindow extends javax.swing.JFrame {
 		textMysqlHost = new javax.swing.JTextField();
 		textMysqlAccount = new javax.swing.JTextField();
 		btnMysqlConnect = new javax.swing.JButton();
+		textMysqlPassword = new javax.swing.JPasswordField();
+		textMysqlDatabase = new javax.swing.JTextField();
+		jPanel2 = new javax.swing.JPanel();
+		jLabel1 = new javax.swing.JLabel();
+		textSearchKeyword = new javax.swing.JTextField();
+		btnSearch = new javax.swing.JButton();
+		jLabel6 = new javax.swing.JLabel();
+		jLabel7 = new javax.swing.JLabel();
+		datetimeStart = new javax.swing.JSpinner();
+		datetimeEnd = new javax.swing.JSpinner();
+		progressStatus = new javax.swing.JProgressBar();
+		jLabel8 = new javax.swing.JLabel();
+		jLabel9 = new javax.swing.JLabel();
+		textTotalCount = new javax.swing.JTextField();
+		jLabel10 = new javax.swing.JLabel();
+		sleeptime = new javax.swing.JSpinner();
+		jLabel11 = new javax.swing.JLabel();
+
+		btnSearch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				btnSearch.setEnabled(false);
+				Thread doFetchWork = new Thread(() -> {
+					HashSet<String> completeSet = new HashSet<String>();
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+					String strSearchUrl = "http://search.appledaily.com.tw/appledaily/search";
+					String strSdate = sdf.format(datetimeStart.getValue());
+					String strEdate = sdf.format(datetimeEnd.getValue());
+
+					// first search step.
+					org.jsoup.Connection firstConn;
+					Document firstDoc;
+					firstConn = Jsoup.connect(strSearchUrl)
+							.timeout(Integer.parseInt(sleeptime.getValue().toString()) * 1000);
+					firstConn.data("searchMode", "Adv").data("searchType", "text").data("select", "AND");
+					firstConn.data("querystrA", textSearchKeyword.getText()).data("sdate", strSdate).data("edate",
+							strEdate);
+
+					try {
+						PreparedStatement stat = connector.getConnection().prepareStatement(
+								"INSERT INTO `appledaily`(`url_guid`, `url`, `date`, `title`, `content`, `keyword`) VALUES(?, ?, ?, ?, ?, ?)");
+						firstDoc = firstConn.post();
+
+						int totalCount = Integer
+								.parseInt(firstDoc.select("#pageNumberSubmit input[name=totalpage]").first().val());
+						progressStatus.setMaximum(totalCount);
+						textTotalCount.setText(String.format("%d", totalCount));
+
+						btnSearch.setEnabled(false);
+						for (Element link : firstDoc.select("#result>li")) {
+							String strUrl = link.select("h2 a").first().absUrl("href");
+							String strGUID = UUID.nameUUIDFromBytes(strUrl.getBytes()).toString();
+
+							if (!guidSet.contains(strGUID)) {
+								Thread.sleep(Integer.parseInt(sleeptime.getValue().toString()) * 1000);
+								Document newsDoc = Jsoup.connect(strUrl).get();
+
+								stat.setString(1, strGUID);
+								stat.setString(2, strUrl);
+								stat.setString(3, link.select("time").text());
+								stat.setString(4, link.select("h2 a").text().trim());
+								stat.setString(5, newsDoc.body().text().trim());
+								stat.setString(6, textSearchKeyword.getText());
+
+								stat.executeUpdate();
+								guidSet.add(strGUID);
+							}
+
+							completeSet.add(strGUID);
+							progressStatus.setValue(completeSet.size());
+							// System.out.printf("\r目前進度： %d / %d 篇",
+							// completeSet.size(), totalCount);
+						}
+
+						int pageIndex = 2;
+						Document resultPageDoc = firstDoc;
+						while (resultPageDoc.select("#pageNumberSubmit ol#result>li").size() == 10) {
+							org.jsoup.Connection pageConn = Jsoup.connect(strSearchUrl)
+									.timeout(Integer.parseInt(sleeptime.getValue().toString()) * 1000);
+							resultPageDoc.select("#pageNumberSubmit input")
+									.forEach(input -> pageConn.data(input.attr("name"), input.val()));
+							pageConn.data("page", Integer.toString(pageIndex++));
+
+							resultPageDoc = pageConn.post();
+
+							for (Element link : resultPageDoc.select("#result>li")) {
+								String strUrl = link.select("h2 a").first().absUrl("href");
+								String strGUID = UUID.nameUUIDFromBytes(strUrl.getBytes()).toString();
+
+								if (!guidSet.contains(strGUID)) {
+									Thread.sleep(Integer.parseInt(sleeptime.getValue().toString()) * 1000);
+									Document newsDoc = Jsoup.connect(strUrl).get();
+
+									stat.setString(1, strGUID);
+									stat.setString(2, strUrl);
+									stat.setString(3, link.select("time").text());
+									stat.setString(4, link.select("h2 a").text().trim());
+									stat.setString(5, newsDoc.body().text().trim());
+									stat.setString(6, textSearchKeyword.getText());
+
+									stat.executeUpdate();
+									guidSet.add(strGUID);
+								}
+
+								completeSet.add(strGUID);
+								progressStatus.setValue(completeSet.size());
+								// System.out.printf("\r目前進度： %d / %d 篇",
+								// completeSet.size(), totalCount);
+							}
+						}
+
+						JOptionPane.showMessageDialog(null, "資料下載完成！", "訊息", JOptionPane.INFORMATION_MESSAGE);
+						stat.close();
+						btnSearch.setEnabled(true);
+					} catch (IOException | InterruptedException | SQLException e1) {
+						e1.printStackTrace();
+						JOptionPane.showMessageDialog(null, "搜尋過程發生錯誤", "錯誤", JOptionPane.ERROR_MESSAGE);
+					}
+				});
+
+				doFetchWork.start();
+			}
+		});
+
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		setTitle("蘋果日報新聞爬蟲");
+
+		jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+		jLabel2.setText("MySQL 連線IP：");
+
+		jLabel3.setText("登入帳號：");
+		jLabel3.setName(""); // NOI18N
+
+		jLabel4.setText("登入密碼：");
+
+		jLabel5.setText("資料庫名稱：");
+
+		btnMysqlConnect.setText("連線");
 		btnMysqlConnect.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -112,138 +250,6 @@ public class AppWindow extends javax.swing.JFrame {
 				}
 			}
 		});
-		textMysqlPassword = new javax.swing.JPasswordField();
-		textMysqlDatabase = new javax.swing.JTextField();
-		jPanel2 = new javax.swing.JPanel();
-		jLabel1 = new javax.swing.JLabel();
-		textSearchKeyword = new javax.swing.JTextField();
-		btnSearch = new javax.swing.JButton();
-
-		btnSearch.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				btnSearch.setEnabled(false);
-				Thread doFetchWork = new Thread(() -> {
-					HashSet<String> completeSet = new HashSet<String>();
-					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-					String strSearchUrl = "http://search.appledaily.com.tw/appledaily/search";
-					String strSdate = sdf.format(datetimeStart.getValue());
-					String strEdate = sdf.format(datetimeEnd.getValue());
-
-					// first search step.
-					org.jsoup.Connection firstConn;
-					Document firstDoc;
-					firstConn = Jsoup.connect(strSearchUrl);
-					firstConn.data("searchMode", "Adv").data("searchType", "text").data("select", "AND");
-					firstConn.data("querystrA", textSearchKeyword.getText()).data("sdate", strSdate).data("edate",
-							strEdate);
-
-					try {
-						PreparedStatement stat = connector.getConnection().prepareStatement(
-								"INSERT INTO `appledaily`(`url_guid`, `url`, `date`, `title`, `content`, `keyword`) VALUES(?, ?, ?, ?, ?, ?)");
-						firstDoc = firstConn.post();
-
-						int totalCount = Integer
-								.parseInt(firstDoc.select("#pageNumberSubmit input[name=totalpage]").first().val());
-						progressStatus.setMaximum(totalCount);
-
-						btnSearch.setEnabled(false);
-						for (Element link : firstDoc.select("#result>li")) {
-							String strUrl = link.select("h2 a").first().absUrl("href");
-							String strGUID = UUID.nameUUIDFromBytes(strUrl.getBytes()).toString();
-
-							if (!guidSet.contains(strGUID)) {
-								Thread.sleep(500);
-								Document newsDoc = Jsoup.connect(strUrl).get();
-
-								stat.setString(1, strGUID);
-								stat.setString(2, strUrl);
-								stat.setString(3, link.select("time").text());
-								stat.setString(4, link.select("h2 a").text().trim());
-								stat.setString(5, newsDoc.body().text().trim());
-								stat.setString(6, textSearchKeyword.getText());
-
-								stat.executeUpdate();
-								guidSet.add(strGUID);
-							}
-
-							completeSet.add(strGUID);
-							progressStatus.setValue(completeSet.size());
-							// System.out.printf("\r目前進度： %d / %d 篇",
-							// completeSet.size(), totalCount);
-						}
-
-						int pageIndex = 2;
-						Document resultPageDoc = firstDoc;
-						while (resultPageDoc.select("#pageNumberSubmit ol#result>li").size() == 10) {
-							org.jsoup.Connection pageConn = Jsoup.connect(strSearchUrl);
-							resultPageDoc.select("#pageNumberSubmit input")
-									.forEach(input -> pageConn.data(input.attr("name"), input.val()));
-							pageConn.data("page", Integer.toString(pageIndex++));
-
-							resultPageDoc = pageConn.post();
-
-							for (Element link : resultPageDoc.select("#result>li")) {
-								String strUrl = link.select("h2 a").first().absUrl("href");
-								String strGUID = UUID.nameUUIDFromBytes(strUrl.getBytes()).toString();
-
-								if (!guidSet.contains(strGUID)) {
-									Thread.sleep(500);
-									Document newsDoc = Jsoup.connect(strUrl).get();
-
-									stat.setString(1, strGUID);
-									stat.setString(2, strUrl);
-									stat.setString(3, link.select("time").text());
-									stat.setString(4, link.select("h2 a").text().trim());
-									stat.setString(5, newsDoc.body().text().trim());
-									stat.setString(6, textSearchKeyword.getText());
-
-									stat.executeUpdate();
-									guidSet.add(strGUID);
-								}
-
-								completeSet.add(strGUID);
-								progressStatus.setValue(completeSet.size());
-								// System.out.printf("\r目前進度： %d / %d 篇",
-								// completeSet.size(), totalCount);
-							}
-						}
-
-						JOptionPane.showMessageDialog(null, "資料下載完成！", "訊息", JOptionPane.INFORMATION_MESSAGE);
-						stat.close();
-						btnSearch.setEnabled(true);
-					} catch (IOException | InterruptedException | SQLException e1) {
-						e1.printStackTrace();
-						JOptionPane.showMessageDialog(null, "搜尋過程發生錯誤", "錯誤", JOptionPane.ERROR_MESSAGE);
-					}
-				});
-
-				doFetchWork.start();
-			}
-		});
-		jLabel6 = new javax.swing.JLabel();
-		jLabel7 = new javax.swing.JLabel();
-		datetimeStart = new javax.swing.JSpinner();
-		datetimeEnd = new javax.swing.JSpinner();
-		progressStatus = new javax.swing.JProgressBar();
-		jLabel8 = new javax.swing.JLabel();
-
-		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-		setTitle("蘋果日報新聞爬蟲");
-
-		jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
-
-		jLabel2.setText("MySQL 連線IP：");
-
-		jLabel3.setText("登入帳號：");
-		jLabel3.setName(""); // NOI18N
-
-		jLabel4.setText("登入密碼：");
-
-		jLabel5.setText("資料庫名稱：");
-
-		btnMysqlConnect.setText("連線");
 
 		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
 		jPanel1.setLayout(jPanel1Layout);
@@ -301,10 +307,18 @@ public class AppWindow extends javax.swing.JFrame {
 		progressStatus.setToolTipText("");
 		progressStatus.setValue(0);
 		progressStatus.setStringPainted(true);
-		// progressStatus.setVisible(false);
 
 		jLabel8.setText("寫入資料庫-完成進度：");
-		// jLabel8.setVisible(false);
+
+		jLabel9.setText("搜尋結果總數：");
+
+		textTotalCount.setEditable(false);
+
+		jLabel10.setText("每則新聞下載間隔：");
+
+		sleeptime.setModel(new javax.swing.SpinnerNumberModel(3, 1, null, 1));
+
+		jLabel11.setText("秒");
 
 		javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
 		jPanel2.setLayout(jPanel2Layout);
@@ -321,23 +335,39 @@ public class AppWindow extends javax.swing.JFrame {
 														200, javax.swing.GroupLayout.PREFERRED_SIZE)
 												.addGap(9, 9, 9).addComponent(btnSearch))
 										.addGroup(jPanel2Layout.createSequentialGroup().addGroup(jPanel2Layout
-												.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-												.addComponent(jLabel8)
-												.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout
-														.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-														.addComponent(jLabel6).addComponent(jLabel7)))
-												.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+												.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+												.addComponent(jLabel9)
 												.addGroup(jPanel2Layout
 														.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+														.addComponent(jLabel8)
+														.addGroup(javax.swing.GroupLayout.Alignment.TRAILING,
+																jPanel2Layout
+																		.createParallelGroup(
+																				javax.swing.GroupLayout.Alignment.LEADING)
+																		.addComponent(jLabel6).addComponent(jLabel7))
+														.addComponent(jLabel10,
+																javax.swing.GroupLayout.Alignment.TRAILING)))
+												.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+												.addGroup(jPanel2Layout
+														.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING,
+																false)
 														.addComponent(progressStatus,
-																javax.swing.GroupLayout.PREFERRED_SIZE, 180,
-																javax.swing.GroupLayout.PREFERRED_SIZE)
+																javax.swing.GroupLayout.DEFAULT_SIZE,
+																javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 														.addComponent(datetimeEnd,
 																javax.swing.GroupLayout.PREFERRED_SIZE, 180,
 																javax.swing.GroupLayout.PREFERRED_SIZE)
 														.addComponent(datetimeStart,
 																javax.swing.GroupLayout.PREFERRED_SIZE, 180,
-																javax.swing.GroupLayout.PREFERRED_SIZE))))
+																javax.swing.GroupLayout.PREFERRED_SIZE)
+														.addComponent(textTotalCount)
+														.addGroup(jPanel2Layout.createSequentialGroup()
+																.addComponent(sleeptime,
+																		javax.swing.GroupLayout.PREFERRED_SIZE, 50,
+																		javax.swing.GroupLayout.PREFERRED_SIZE)
+																.addPreferredGap(
+																		javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+																.addComponent(jLabel11)))))
 										.addGap(28, 28, 28)));
 		jPanel2Layout.setVerticalGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 				.addGroup(jPanel2Layout.createSequentialGroup().addContainerGap()
@@ -356,11 +386,22 @@ public class AppWindow extends javax.swing.JFrame {
 								.addComponent(jLabel7).addComponent(datetimeEnd, javax.swing.GroupLayout.PREFERRED_SIZE,
 										javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
 						.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+						.addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+								.addComponent(jLabel10)
+								.addComponent(sleeptime, javax.swing.GroupLayout.PREFERRED_SIZE,
+										javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+								.addComponent(jLabel11))
+						.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+						.addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+								.addComponent(jLabel9).addComponent(textTotalCount,
+										javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE,
+										javax.swing.GroupLayout.PREFERRED_SIZE))
+						.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
 						.addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 								.addComponent(progressStatus, javax.swing.GroupLayout.PREFERRED_SIZE,
 										javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
 								.addComponent(jLabel8))
-						.addContainerGap(27, Short.MAX_VALUE)));
+						.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
 
 		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
 		getContentPane().setLayout(layout);
@@ -426,6 +467,8 @@ public class AppWindow extends javax.swing.JFrame {
 	private javax.swing.JSpinner datetimeStart;
 	private javax.swing.JButton btnSearch;
 	private javax.swing.JLabel jLabel1;
+	private javax.swing.JLabel jLabel10;
+	private javax.swing.JLabel jLabel11;
 	private javax.swing.JLabel jLabel2;
 	private javax.swing.JLabel jLabel3;
 	private javax.swing.JLabel jLabel4;
@@ -433,13 +476,16 @@ public class AppWindow extends javax.swing.JFrame {
 	private javax.swing.JLabel jLabel6;
 	private javax.swing.JLabel jLabel7;
 	private javax.swing.JLabel jLabel8;
+	private javax.swing.JLabel jLabel9;
 	private javax.swing.JPanel jPanel1;
 	private javax.swing.JPanel jPanel2;
 	private javax.swing.JProgressBar progressStatus;
+	private javax.swing.JSpinner sleeptime;
 	private javax.swing.JTextField textMysqlAccount;
 	private javax.swing.JTextField textMysqlDatabase;
 	private javax.swing.JTextField textMysqlHost;
 	private javax.swing.JPasswordField textMysqlPassword;
 	private javax.swing.JTextField textSearchKeyword;
+	private javax.swing.JTextField textTotalCount;
 	// End of variables declaration//GEN-END:variables
 }
